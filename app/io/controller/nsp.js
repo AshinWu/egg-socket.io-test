7edc891ebf1d1628d6b0b15c6e62582a0165a266
ashinWu <1571676564@qq.com>
2019-12-28 15:24:46 +0800

    update nsp.js comment

--- app/io/controller/nsp.js
+++ app/io/controller/nsp.js
@@ -9,10 +9,12 @@ class NspController extends Controller {
     const socket = ctx.socket;
     try {
       socket.emit('res', message);
-      // 这里console不出，为什么?
+      // qs1:这里console不出，为什么?
       console.log('into exchange()...');
+      
+      // qs2: socket.io test怎么mocksession? 试过app.mockSession({})无效?
       if (ctx.session.user) {
-        // do someing...
+        // do something...
       }
     } catch (error) {
       app.logger.error(error);
